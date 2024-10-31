099a2fd256afe332d8d8176d3e238b6abf03a38b
Richard Nelson <r3nelson@outlook.com>
2024-10-30 23:48:45 -1000

    added Random button that fetches 50 random images. Added error handling to displayImages

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// elements
+// HTML elements
 const selectBreed = document.getElementById("breed");
 const imgCount = document.getElementById("img-count");
 const imagesConatiner = document.getElementById("images-container");
@@ -8,6 +8,7 @@ const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const squareOne = document.getElementById("square-1");
 const squareTwo = document.getElementById("square-2");
+const random = document.getElementById("random");
 
 // global variables
 let displayNum = +imgCount.value;
@@ -15,6 +16,7 @@ let cur_index = 0;
 let end_index = 10;
 let images = null;
 
+// fetch requests
 async function getAllBreeds(url = "https://dog.ceo/api/breeds/list/all") {
   const res = await fetch(url);
   const data = await res.json();
@@ -22,13 +24,6 @@ async function getAllBreeds(url = "https://dog.ceo/api/breeds/list/all") {
   const breeds = Object.keys(data.message);
   return breeds;
 }
-
-function setEndIndex(pics) {
-  end_index = pics;
-  updateSquareOne();
-  squareTwo.textContent = end_index;
-}
-
 async function getImagesByBreed(
   breed,
   url = `https://dog.ceo/api/breed/${breed}/images`
@@ -37,30 +32,45 @@ async function getImagesByBreed(
   const img = await res.json();
   return img.message;
 }
+async function selectRandomImages(numImages) {
+  if (numImages > 50) return `numImages must be <=50\nnumImgaes = ${numImages}`;
+  url = `https://dog.ceo/api/breeds/image/random/${numImages}`;
+  const res = await fetch(url);
+  images_object = await res.json();
+  images = images_object.message;
+  return images;
+}
 
+// updater functions
+function setEndIndex(pics) {
+  end_index = pics;
+  updateSquareOne();
+  squareTwo.textContent = end_index;
+}
 function clearImages() {
   imagesConatiner.innerHTML = "";
 }
-
 function updateSquareOne() {
   squareOne.textContent = cur_index + 1;
 }
-
 function resetStartIndex() {
   cur_index = 0;
 }
-
 function updateDisplay() {
   clearImages();
   let i = cur_index;
   while (i < cur_index + displayNum) {
+    if (i >= end_index) break;
     const img = document.createElement("img");
     img.src = images[i];
     imagesConatiner.appendChild(img);
     i++;
   }
 }
-
+async function setBreedType(breed) {
+  if (!breed) return;
+  breedType.innerHTML = "Breed: " + breed[0].toUpperCase() + breed.slice(1);
+}
 async function displayImages() {
   if (displayNum > 10)
     return `displayNum must be <= 10\ndisplayNum: ${displayNum}`;
@@ -73,17 +83,6 @@ async function displayImages() {
   }
   updateDisplay();
 }
-
-async function selectRandomImages(
-  numImages,
-  url = `https://dog.ceo/api/breeds/image/random/${numImages}`
-) {
-  if (numImages > 50) return `numImages must be <=50\nnumImgaes = ${numImages}`;
-  const res = await fetch(url);
-  images = await res.json();
-  return images;
-}
-
 async function addBreedsToSelect() {
   const breeds = await getAllBreeds();
   breeds.forEach((breed) => {
@@ -94,14 +93,9 @@ async function addBreedsToSelect() {
   });
 }
 
-async function setBreedType(breed) {
-  if (!breed) return;
-  breedType.innerHTML = "Breed: " + breed[0].toUpperCase() + breed.slice(1);
-}
-
 // event handler functions
 function handleNext() {
-  if (cur_index + displayNum === end_index) return;
+  if (cur_index + displayNum >= end_index) return;
   cur_index += displayNum;
   updateSquareOne();
   (async () => {
@@ -126,33 +120,37 @@ function handleChangeImgCount(event) {
     })();
   }
 }
+async function handleBreedChange() {
+  setEndIndex(images.length);
+  updateSquareOne();
+  await displayImages();
+}
+async function handleRandom(numImages) {
+  images = await selectRandomImages(numImages);
+  if (images) handleBreedChange();
+  else console.error("Error fetching random images");
+}
 async function handleSelectBreed(event) {
   resetStartIndex();
   const breed = event.target.value;
   await setBreedType(breed);
   images = await getImagesByBreed(breed);
-  if (images) {
-    setEndIndex(images.length);
-    // cur_index = 1;
-    updateSquareOne();
-    await displayImages();
-  } else console.error("No images to display");
+  if (images) handleBreedChange();
+  else console.error("No images to display");
 }
 
 //event listeners
+random.addEventListener("click", () => handleRandom(50));
+next.addEventListener("click", handleNext);
+prev.addEventListener("click", handlePrev);
 imgCount.addEventListener("change", (event) => {
   handleChangeImgCount(event);
 });
-
 selectBreed.addEventListener("change", (event) => {
   handleSelectBreed(event);
 });
 
-next.addEventListener("click", handleNext);
-prev.addEventListener("click", handlePrev);
-
 function main() {
   addBreedsToSelect();
 }
-
 main();
