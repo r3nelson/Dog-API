2956b0b84c3163cadfdef6c7e88650dd298830a3
Richard Nelson <r3nelson@outlook.com>
2024-10-30 15:34:38 -1000

    added functionality for next and prev buttons

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const next = document.getElementById("next");
 const squareOne = document.getElementById("square-1");
 const squareTwo = document.getElementById("square-2");
 
+// global variables
 let displayNum = +imgCount.value;
-let cur_index = +squareOne.textContent;
+let cur_index = 0;
 let end_index = 10;
 let images = null;
 
@@ -22,9 +23,9 @@ async function getAllBreeds(url = "https://dog.ceo/api/breeds/list/all") {
   return breeds;
 }
 
-function setTotalPics(pics) {
+function setEndIndex(pics) {
   end_index = pics;
-  squareOne.textContent = cur_index;
+  updateSquareOne();
   squareTwo.textContent = end_index;
 }
 
@@ -39,6 +40,10 @@ async function getImagesByBreed(
 
 function clearImages() {
   imagesConatiner.innerHTML = "";
+}
+
+function updateSquareOne() {
+  squareOne.textContent = cur_index + 1;
 }
 
 function resetStartIndex() {
@@ -57,9 +62,6 @@ function updateDisplay() {
 }
 
 async function displayImages() {
-  console.log(cur_index);
-  console.log(typeof cur_index);
-
   if (displayNum > 10)
     return `displayNum must be <= 10\ndisplayNum: ${displayNum}`;
   if (!images) return;
@@ -69,14 +71,7 @@ async function displayImages() {
   if (images.length < displayNum) {
     displayNum = images.length;
   }
-
-  console.log(cur_index);
-  console.log(typeof cur_index);
-  console.log(images);
   updateDisplay();
-  console.log(cur_index);
-  console.log(typeof cur_index);
-  console.log(images);
 }
 
 async function selectRandomImages(
@@ -104,33 +99,44 @@ async function setBreedType(breed) {
   breedType.innerHTML = "Breed: " + breed[0].toUpperCase() + breed.slice(1);
 }
 
-function handlePrev() {
-  cur_index -= displayNum;
-  if (cur_index < 1) cur_index = 1;
-}
-
+// event handler functions
 function handleNext() {
-  if (cur_index + displayNum > end_index) cur_index = end_index - displayNum;
-  else cur_index += displayNum;
+  if (cur_index + displayNum === end_index) return;
+  cur_index += displayNum;
+  updateSquareOne();
+  (async () => {
+    await displayImages();
+  })();
 }
+function handlePrev() {
+  if (cur_index - displayNum <= 0) {
+    cur_index = 0;
+  } else cur_index -= displayNum;
 
+  updateSquareOne();
+  (async () => {
+    await displayImages();
+  })();
+}
 function handleChangeImgCount(event) {
   displayNum = +event.target.value;
   if (images) {
     (async () => {
-      console.log("about to display images");
       await displayImages();
     })();
   }
 }
-
 async function handleSelectBreed(event) {
   resetStartIndex();
   const breed = event.target.value;
   await setBreedType(breed);
   images = await getImagesByBreed(breed);
-  setTotalPics(images.length);
-  await displayImages();
+  if (images) {
+    setEndIndex(images.length);
+    // cur_index = 1;
+    updateSquareOne();
+    await displayImages();
+  } else console.error("No images to display");
 }
 
 //event listeners
@@ -143,6 +149,7 @@ selectBreed.addEventListener("change", (event) => {
 });
 
 next.addEventListener("click", handleNext);
+prev.addEventListener("click", handlePrev);
 
 function main() {
   addBreedsToSelect();
